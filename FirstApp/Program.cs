ae8912ce1cdeaaa0811650756f09c53a715cc51a
Michael M. <trdMichaelM@yandex.ru>
2024-02-12 15:39:27 +0300

    virtual/override

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,22 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class Person { }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual string GetInfo()
+        {
+            return $"Имя: {Name}, Возраст: {Age}";
+        }
+    }
+
+    public class Student : Person
+    {
+        public int Course { get; set; }
+        public string Profession { get; set; }
+
+        public override string GetInfo()
+        {
+            return $"{base.GetInfo()}, Курс: {Course}, Специальность: {Profession}";
+        }
+
+    }
+    public class ProgrammingStudent : Student
+    {
+        public string ProgrammingLanguage { get; set; }
+
+        public override string GetInfo()
+        {
+            return $"{base.GetInfo()}, язык программирования: {ProgrammingLanguage}";
+        }
+    }
+
+    public class Professor : Person
+    {
+        public string Subject { get; set; }
+        public override string GetInfo()
+        {
+            return $"{base.GetInfo()}, предмет: {Subject}";
+        }
+    }
+
     class Program
     {
         static void Main()
         {
-            Person person = new Person();
+            List<Person> people = new List<Person>();
+            people.Add(new Student() { Name = "Student1", Age = 21, Course = 1, Profession = "C#" });
+            people.Add(new Professor() { Name = "Professor1", Age = 51, Subject = "Матан" });
+            people.Add(new ProgrammingStudent() { Name = "ProgrammingStudent1", Age = 22, Course = 2, Profession = "ЯП", ProgrammingLanguage = "C#" });
 
-            object obj = person; // upcasting
-            Console.WriteLine(obj);
-
-            person = obj as Person; // downcasting
-            Console.WriteLine(obj);
-            if (obj is Person)
-                person = (Person)obj; // downcasting
-            Console.WriteLine(obj);
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.GetInfo());
+            }
         }
     }
 }

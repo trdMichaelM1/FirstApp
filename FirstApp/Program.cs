328e918bff9836560f1f351e6cab967532917755
Michael M. <trdMichaelM@yandex.ru>
2024-02-12 13:11:22 +0300

    Наследование - Итоговый тест

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,11 +2,37 @@
 
 namespace CSharpEssentials
 {
+    public class Student
+    {
+        public string Password { get; set; } = "Алоомора";
+
+        public void Spell() { }
+    }
+
+    public class GriffindorStudent : Student
+    {
+        public new string Password { get; set; } = "Грата Домум";
+
+        public void BreakRules() { }
+    }
+
+    public class SlytherinStudent : Student
+    {
+        public new string Password { get; set; } = "Чистая кровь";
+
+        public void Cheat() { }
+    }
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, world!!!");
+            Student stud = new Student();
+            Console.WriteLine(stud.Password);//Алоомора
+
+            GriffindorStudent grifStud = new GriffindorStudent();
+            grifStud.BreakRules(); //ничего не выводит
+            Console.WriteLine(grifStud.Password); //Грата Домум
+            Console.WriteLine(((Student)grifStud).Password); //Алоомора
         }
     }
 }

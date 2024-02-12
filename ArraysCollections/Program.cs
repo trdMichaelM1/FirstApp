01ba02aee027b5b8841765caebb3b174c1116680
Michael <Michael@DELL-WIN>
2024-02-12 12:29:10 +0300

    Add project files.

---
+++ ArraysCollections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+            int[] arr2 = new int[] { };
+
+            Array myArray = new int[] { 1, 2 };
+            Array myArray2 = Array.CreateInstance(typeof(int), 5);
+            myArray2.SetValue(12, 0);
+            Console.WriteLine(myArray2.GetValue(0));
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of Fibonacci numbers you want to generate: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] fibonacci = new int[n];
+
+            int a0 = 0;
+            int a1 = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = a0 + a1;
+                fibonacci[i] = a;
+
+                a0 = a1;
+                a1 = a;
+            }
+
+            Console.WriteLine(string.Join(" ", fibonacci));
+        }
+    }
+}

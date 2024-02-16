fcaaa495d2e28bc890b5e9eececba9c0e10f0176
Michael M. <trdMichaelM@yandex.ru>
2024-02-16 18:40:59 +0300

    Read and Write File and a new project was added

---
+++ ReadWriteFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ReadWriteFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+            string text = "Hello, Michael M.";
+            string path = $@"{currentDirectory}\note.txt";
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                fs.Write(array, 0, array.Length);
+            }
+            Console.ReadKey();
+
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] array = new byte[fs.Length];
+                fs.Read(array, 0, array.Length);
+                text = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"From file: {text}");
+            }
+        }
+    }
+}

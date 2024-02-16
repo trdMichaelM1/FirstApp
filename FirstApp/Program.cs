50bbadc0e60b6f51ba2f58433496deefda4da407
Michael M. <trdMichaelM@yandex.ru>
2024-02-16 18:00:27 +0300

    Работа с файлами

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,101 +7,47 @@ namespace CSharpEssentials
     {
         static void Main()
         {
-            DriveInfo[] drives = DriveInfo.GetDrives();
-
-            foreach (DriveInfo drive in drives)
+            // Получение информации о файле
+            string path = @"C:\TestFiles\test1.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
             {
-                Console.WriteLine($"Название: {drive.Name}");
-                if (drive.IsReady)
-                {
-                    Console.WriteLine($"Тип: {drive.DriveFormat}");
-                    Console.WriteLine($"Объем диска: {drive.TotalSize / 1024 / 1024 / 1024}");
-                    Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace / 1024 / 1024 / 1024}");
-                }
-                Console.WriteLine();
+                Console.WriteLine("Имя файла: {0}", fileInf.Name);
+                Console.WriteLine("Время создания: {0}", fileInf.CreationTime);
+                Console.WriteLine("Размер: {0}", fileInf.Length);
             }
 
-            string dirName = @"C:\Users\Michael";
-
-            if (Directory.Exists(dirName))
+            // Удаление файла
+            path = @"C:\TestFiles\test1.txt";
+            fileInf = new FileInfo(path);
+            if (fileInf.Exists)
             {
-                Console.WriteLine("Подкаталоги:");
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string s in dirs)
-                {
-                    Console.WriteLine(s);
-                }
-                Console.WriteLine();
-                Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);
-                foreach (string s in files)
-                {
-                    Console.WriteLine(s);
-                }
+                fileInf.Delete();
+                // альтернатива с помощью класса File
             }
 
-            // Чтобы поиск происходил в текущем каталоге (где находится файл exe), нужно в dirName указать точку:
-            dirName = ".";
-
-            if (Directory.Exists(dirName))
+            // Перемещение файла
+            path = @"C:\TestFiles\test1.txt";
+            string newPath = @"C:\TestFiles\test2\test1.txt";
+            fileInf = new FileInfo(path);
+            if (fileInf.Exists)
             {
-                Console.WriteLine("Подкаталоги:");
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string s in dirs)
-                {
-                    Console.WriteLine(s);
-                }
-                Console.WriteLine();
-                Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);
-                foreach (string s in files)
-                {
-                    Console.WriteLine(s);
-                }
+                fileInf.MoveTo(newPath);
+                // альтернатива с помощью класса File
+                // File.Move(path, newPath);
             }
 
-            // Создание каталога
-            string path = @"C:\Stream";
-            string subpath = @"Michael\directories";
-            DirectoryInfo dirInfo = new DirectoryInfo(path);
-            if (!dirInfo.Exists)
+
+            // Копирование файла
+            path = @"";
+            newPath = @"";
+            fileInf = new FileInfo(path);
+            if (fileInf.Exists)
             {
-                dirInfo.Create();
+                fileInf.CopyTo(newPath, true);
+                // альтернатива с помощью класса File
+                // File.Copy(path, newPath, true);
             }
-            dirInfo.CreateSubdirectory(subpath);
-
-            // Получение информации о каталоге
-            dirName = @"C:\Program Files\Test";
-
-            dirInfo = new DirectoryInfo(dirName);
-
-            Console.WriteLine($"Название каталога: {dirInfo.Name}");
-            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
-            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
-            Console.WriteLine($"Родительский каталог: {dirInfo.Parent}");
-
-            Console.ReadKey();
-            // Удаление каталога
-            dirName = @"C:\Stream";
-
-            try
-            {
-                dirInfo = new DirectoryInfo(dirName);
-                dirInfo.Delete(true); // папку надо удалять со всем содержимым
-                Console.WriteLine("Каталог удален");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.WriteLine(Environment.MachineName);
-            Console.WriteLine(Environment.UserName);
-            Console.WriteLine(System.IO.Path.GetDirectoryName(typeof(Program).Assembly.Location));
-            Console.WriteLine(System.AppDomain.CurrentDomain.BaseDirectory);
-            Console.WriteLine(System.Environment.CurrentDirectory);
-            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
         }
     }
 }

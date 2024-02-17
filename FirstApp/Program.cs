cc556a5bca49cf44251f2b49a53c761c95a2a5b4
Michael M. <trdMichaelM@yandex.ru>
2024-02-17 16:04:46 +0300

    completed task 8

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,6 +9,7 @@ namespace CSharpEssentials
             Console.WriteLine("Hello, Michael M.");
             // We made some changes here on branch "task_007"
             // one more commit for task_007
+            // made some changes for the task 8 and commited it into master
         }
     }
 }

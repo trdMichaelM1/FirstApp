def5a93f19db5248498d146ad150887517fab51d
Michael M. <trdMichaelM@yandex.ru>
2024-02-17 15:43:17 +0300

    Подготовка в уроку слияния веток

---
+++ A_OOP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_OOP
+{
+    public class Calculator
+    {
+        public double Average(int[] numbers)
+        {
+            double sum = 0;
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            return sum / numbers.Length;
+        }
+
+        public double Average2(params int[] numbers)
+        {
+            double sum = 0;
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            return sum / numbers.Length;
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Calculator calculator = new Calculator();
+                double avg = calculator.Average(new int[] { 1, 2, 3, 4 });
+                avg = calculator.Average2(1, 2, 3, 4);
+                Console.WriteLine(avg);
+
+                string line = Console.ReadLine();
+                bool wasParse = int.TryParse(line, out int number);
+                if (wasParse)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace CSharpEssentials
 {
@@ -7,63 +6,7 @@ namespace CSharpEssentials
     {
         static void Main()
         {
-            // создаем каталог для файла
-            string path = @"C:\TestDir";
-            DirectoryInfo dirInfo = new DirectoryInfo(path);
-            if (!dirInfo.Exists)
-            {
-                dirInfo.Create();
-            }
-            Console.WriteLine("Введите строку для записи в файл:");
-            string text = Console.ReadLine();
-
-            // запись в файл
-            using (FileStream fstream = new FileStream($@"{path}\note.txt", FileMode.OpenOrCreate))
-            {
-                // преобразуем строку в байты
-                byte[] array = System.Text.Encoding.Default.GetBytes(text);
-                // запись массива байтов в файл
-                fstream.Write(array, 0, array.Length);
-                Console.WriteLine("Текст записан в файл");
-            }
-
-            // чтение из файла
-            using (FileStream fstream = File.OpenRead($@"{path}\note.txt"))
-            {
-                // преобразуем строку в байты
-                byte[] array = new byte[fstream.Length];
-                // считываем данные
-                fstream.Read(array, 0, array.Length);
-                // декодируем байты в строку
-                string textFromFile = System.Text.Encoding.Default.GetString(array);
-                Console.WriteLine($"Текст из файла: {textFromFile}");
-            }
-
-            // StreamWriter
-            string writePath = @"";
-
-             text = "Привет мир!\n Hello world";
-            using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
-            {
-                sw.WriteLine(text);
-            }
-
-             writePath = @"";
-
-             text = "Привет мир!\n Hello world";
-            using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
-            {
-                sw.WriteLine("Дозапись");
-                sw.Write(4.5);
-            }
-
-            //StreamReader
-             path = @"";
-
-            using (StreamReader sr = new StreamReader(path))
-            {
-                Console.WriteLine(sr.ReadToEnd());
-            }
+            
         }
     }
 }

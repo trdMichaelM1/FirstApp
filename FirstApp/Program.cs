19a76b0fb601e7b5f7fac50a0536715c053f538b
Michael M. <trdMichaelM@yandex.ru>
2024-02-18 13:50:05 +0300

    анонимные типы

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,32 +2,13 @@
 
 namespace CSharpEssentials
 {
-    public class Company
-    {
-        public string Name { get; set; }
-
-        public Company(string name)
-        {
-            Name = name;
-        }
-    }
     class Program
     {
-        static void Print(Company company)
-        {
-            //if (company == null || company.Name == null)
-            //{
-            //    Console.WriteLine("Неизвестное имя");
-            //}
-            //else
-            //{
-            //    Console.WriteLine(company.Name);
-            //}
-            Console.WriteLine(company?.Name ?? "Неизвестное имя");
-        }
         static void Main()
         {
-
+            var anonymous = new { Name = "Test", Age = 18 };
+            Console.WriteLine(anonymous.Name);
+            Console.WriteLine(anonymous.Age);
         }
     }
 }

da7821db683419f07a6534ea1b813ef71398206f
Michael M. <trdMichaelM@yandex.ru>
2024-02-18 12:18:08 +0300

    Операторы, работающие с NULL

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,14 +2,30 @@
 
 namespace CSharpEssentials
 {
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+
+        public UserProfile(string name)
+        {
+            Name = name;
+        }
+
+        public void DisplayUserInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Пол: {Gender ?? "Пол не указан"}");
+            Console.WriteLine($"Страна: {Country ?? "Страна не указана"}");
+        }
+    }
     class Program
     {
         static void Main()
         {
-            string line = null;
-
-            string name = line ?? throw new ArgumentNullException("line");
-            Console.WriteLine(name);
+            UserProfile usProf = new UserProfile("Алина") { Gender = "Жен" };
+            usProf.DisplayUserInfo();
         }
     }
 }

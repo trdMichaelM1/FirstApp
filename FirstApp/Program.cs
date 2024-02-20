8e5e935b0a5a91e1a7be50cae22a6a84fc44e606
Michael M. <trdMichaelM@yandex.ru>
2024-02-20 14:32:37 +0300

    Boxing/Unboxing

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,23 +2,15 @@
 
 namespace CSharpEssentials
 {
-    struct Point
-    {
-        public int X;
-        public int Y;
-    }
-
-    struct Line
-    {
-        public Point StartPoint;
-        public Point EndPoint;
-    }
     class Program
     {
         static void Main()
         {
-            Line[] lines = new Line[5];
-            Console.WriteLine(lines[0].StartPoint.X);
+            int x = 7;
+            object y = x;
+            //long l = (long)(int)y;
+            long l = (int)y;
+            Console.WriteLine(y);
         }
     }
 }

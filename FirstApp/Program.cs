7357c4e8cbb7341e998afc8dab0c8dfc4cce4b1a
Michael M. <trdMichaelM@yandex.ru>
2024-02-20 15:20:57 +0300

    Обобщенный метод FindOccurrencesCount

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,19 +5,25 @@ namespace CSharpEssentials
 
     class Program
     {
-        public static void PrintArray<T>(T[] array)
+        public static int FindOccurrencesCount<T>(T[] array, T item)
         {
-            foreach(T t in array)
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(t);
+                if (array[i].Equals(item))
+                    count++;
             }
+            return count;
         }
-
         static void Main()
         {
-            string[] names = { "Josef", "Vadim", "Ivan" };
-            PrintArray(names);
+            int[] numbers = { 1, 2, 3, 2, 4, 2 };
+            int occurrences = FindOccurrencesCount(numbers, 2);
+            Console.WriteLine(occurrences);   // 3
 
+            string[] names = { "Josef", "Vadim", "Josef", "Ivan" };
+            int occurrences2 = FindOccurrencesCount(names, "Josef");
+            Console.WriteLine(occurrences2);  // 2
         }
     }
 }

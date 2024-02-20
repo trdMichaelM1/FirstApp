df755ce03804b86e89eab9c450cc9d5a64d5f024
Michael M. <trdMichaelM@yandex.ru>
2024-02-20 18:30:22 +0300

    Много методов в делегате

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -3,65 +3,26 @@ using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public User(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name}, {Age}";
-        }
-    }
-
+    delegate void Method(int x);
     class Program
     {
-        // шаблон правила
-        delegate void Rule(User user);
-
-        // правило-метод LongNameRule
-        private static void LongNameRule(User user)
-        {
-            if (user.Name.Length > 10)
-                user.Name = user.Name.Substring(0, 10);
-        }
-
-        private static void IncreaseAge(User user)
-        {
-            user.Age++;
-        }
-
-        private static List<User> TransformUsersByRule(List<User> users, Rule rule)
-        {
-            foreach (var user in users)
-            {
-                rule(user);
-            }
-            return users;
-        }
-
         static void Main()
         {
-            List<User> users = new List<User>
-            {
-                new User("namenamename", 25),
-                new User("1234567890", 34),
-                new User("123456", 14),
-            };
+            Method method1 = M1;
+            method1 += M2;
+            method1 += M1;
+            method1 -= M2;
+        
+        }
 
-            List<User> transformedUsers = TransformUsersByRule(users, LongNameRule);
-            transformedUsers = TransformUsersByRule(users, IncreaseAge);
+        private static void M1(int x)
+        {
+            Console.WriteLine($"M1: x={x}");
+        }
 
-            foreach (var user in transformedUsers)
-            {
-                Console.WriteLine(user);
-            }
+        private static void M2(int x)
+        {
+            Console.WriteLine($"M2: x={x}");
         }
     }
 }

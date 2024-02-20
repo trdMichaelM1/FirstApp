fd97846c18e175e2ae405d252b336d40b5bb7e36
Michael M. <trdMichaelM@yandex.ru>
2024-02-20 18:49:24 +0300

    Оператор условного NULL в делегатах

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,69 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public User(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name}, {Age}";
-        }
-    }
-
     class Program
     {
-        // шаблон правила
-        delegate void Rule(User user);
+        delegate void MyDelegate();
 
-        // правило-метод LongNameRule
-        private static void LongNameRule(User user)
+        private static void Method1()
         {
-            if (user.Name.Length > 10)
-                user.Name = user.Name.Substring(0, 10);
+            Console.Write("Hello");
         }
 
-        private static void IncreaseAge(User user)
+        private static void Method2()
         {
-            user.Age++;
+            Console.Write("Hi");
         }
 
-        private static List<User> TransformUsersByRule(List<User> users, Rule rule)
+        static void Main(string[] args)
         {
-            foreach (var user in users)
-            {
-                rule(user);
-            }
-            return users;
-        }
+            MyDelegate myDelegate = Method1;
+            myDelegate += Method2;
+            myDelegate -= Method1;
+            myDelegate -= Method2;
 
-        static void Main()
-        {
-            List<User> users = new List<User>
-            {
-                new User("namenamename", 25),
-                new User("1234567890", 34),
-                new User("123456", 14),
-            };
-
-            Rule rules = LongNameRule;
-            rules += IncreaseAge;
-
-            List<User> transformedUsers = TransformUsersByRule(users, rules);
-
-            foreach (var user in transformedUsers)
-            {
-                Console.WriteLine(user);
-            }
+            myDelegate?.Invoke();
         }
     }
 }

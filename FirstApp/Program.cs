4db7d42332ffd9be2e1e37e4ceb497ff1e61aa39
Michael M. <trdMichaelM@yandex.ru>
2024-02-22 15:48:16 +0300

    Отбираем числа по некоторому условию 3

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,10 +7,8 @@ namespace CSharpEssentials
     {
         static void Main()
         {
-            Func<int, bool> predicate = item =>
-            {
-                return item * item % 10 == 9 ? true : false;
-            };
+            Func<int, bool> predicate = item => item * 2 % 10 == 6;
+
 
             Print(new List<int> { 1, 2, 3, 4, 5, 7, 13, 27 }, predicate);
         }

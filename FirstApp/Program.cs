90c9503a82b096b8b09e279ea8911dabe9efd3db
Michael M. <trdMichaelM@yandex.ru>
2024-02-22 15:55:47 +0300

    Фильтр

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,27 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CSharpEssentials
+namespace Stepik
 {
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Func<int, bool> predicate = item => item * 2 % 10 == 6;
-
-
-            Print(new List<int> { 1, 2, 3, 4, 5, 7, 13, 27 }, predicate);
-        }
-
-        static void Print(List<int> items, Func<int, bool> predicate)
-        {
-            foreach (var item in items)
+            List<User> users = new List<User>
             {
-                if (predicate(item))
+                new User { Name = "Alice", Age = 25 },
+                new User { Name = "Bob", Age = 30 },
+                new User { Name = "Charlie", Age = 35 }
+            };
+
+            List<User> filteredUsers = FilterUsers(users, u => u.Age > 25);
+
+            foreach (var user in filteredUsers)
+            {
+                Console.WriteLine($"{user.Name} - {user.Age}");
+            }
+        }
+        static List<User> FilterUsers(List<User> users, Func<User, bool> filterCriteria)
+        {
+            List<User> filteredUsers = new List<User>();
+            foreach (var user in users)
+            {
+                if (filterCriteria(user))
                 {
-                    Console.Write(item + " ");
+                    filteredUsers.Add(user);
                 }
             }
+            return filteredUsers;
         }
     }
 }

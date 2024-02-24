27bcab87d7a14a49c664a2d74e8792e3f3a95622
Michael M. <trdMichaelM@yandex.ru>
2024-02-24 11:15:19 +0300

    Практическое применение делегатов

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,52 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace Stepik
+namespace CSharpEssentials
 {
-    public class School
+    public class User
     {
-        public List<Student> Students;
+        private string name;
+        private int age;
 
-        public int Count(Func<Student, bool> filter)
+        public Action<string> MessageHandler;
+        
+        public void SetAge(string input)
         {
-            int count = 0;
-            foreach (Student student in Students)
+            if (int.TryParse(input, out int age))
             {
-                if(filter(student))
-                    count++;
+                if (age > 18 && age < 100)
+                {
+                    this.age = age;
+                    MessageHandler?.Invoke("Возраст записан");
+                }
+                else
+                {
+                    MessageHandler?.Invoke("Возраст должен быть числом между 18 и 100");
+                }
             }
-            return count;
+            else
+            {
+                MessageHandler?.Invoke("Возраст должен быть числом");
+            }
         }
     }
-
-    public class Student
-    {
-        public readonly string FIO;
-        public readonly int Grade;
-        public readonly double Performance;
-
-        public Student(string fio, int grade, double performance)
-        {
-            FIO = fio;
-            Grade = grade;
-            Performance = performance;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            School school = new School();
-            school.Students = new List<Student>()
-            {
-                new Student("Багаев Аслан", 10, 4.3),
-                new Student("Абаев Георгий", 8, 5),
-                new Student("Дзуцев Михаил", 2, 3.8),
-                new Student("Елоев Сармат", 11, 4.6)
-            };
-            Console.WriteLine(school.Count(x => x.Grade > 2)); // 3
-            Console.WriteLine(school.Count(x => x.FIO.Contains("Багаев"))); // 1
+            User user = new User();
+            user.MessageHandler = ShowOnConsole;
+            user.MessageHandler += SendEmail;
+            user.SetAge(Console.ReadLine());
+        }
+
+        private static void ShowOnConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void SendEmail(string message)
+        {
+            Console.WriteLine($"Send an emain \"{message}\"");
         }
     }
 }

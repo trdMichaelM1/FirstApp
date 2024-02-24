b4cd36c984e946500ef577c35bd5d502d55caa03
Michael M. <trdMichaelM@yandex.ru>
2024-02-24 13:20:43 +0300

    Регистрация пользователя

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,73 +1,113 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class SetAgeEventArgs : EventArgs
+    public static class CodeGenerator
     {
-        public string Message { get; }
-
-        public SetAgeEventArgs(string message)
+        public static int Get()
         {
-            Message = message;
+            return new Random().Next(1000, 9999);
         }
     }
-
     public class User
     {
-        private string name;
-        private int age;
+        public string Name;
+        public string Email;
+        public string Password;
 
-        //public event Action<string> MessageHandler;
-        public event EventHandler<SetAgeEventArgs> MessageHandler;
-
-        public void SetAge(string input)
+        public User(string name, string email, string password)
         {
-            if (int.TryParse(input, out int age))
-            {
-                if (age > 18 && age < 100)
-                {
-                    this.age = age;
-                    MessageHandler?.Invoke(this, new SetAgeEventArgs("Возраст записан"));
-                }
-                else
-                {
-                    MessageHandler?.Invoke(this, new SetAgeEventArgs("Возраст должен быть числом между 18 и 100"));
-                }
-            }
-            else
-            {
-                MessageHandler?.Invoke(this, new SetAgeEventArgs("Возраст должен быть числом"));
-            }
+            Name = name;
+            Email = email;
+            Password = password;
         }
     }
+
+    public class UserRegisteredEventArgs : EventArgs
+    {
+        public string Email { get; }
+
+        public UserRegisteredEventArgs(string email)
+        {
+            Email = email;
+        }
+    }
+
+    public class UserManager
+    {
+        public List<User> Users { get; } = new List<User>();
+        public event EventHandler<UserRegisteredEventArgs> UserRegistered;
+
+        public void Register(User user)
+        {
+            Users.Add(user);
+            UserRegistered?.Invoke(this, new UserRegisteredEventArgs(user.Email));
+        }
+    }
+
+    public class TwoFactorAuthentication
+    {
+        private UserManager userManager { get; }
+
+        public TwoFactorAuthentication(UserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        private void SendCode(object sender, UserRegisteredEventArgs e)
+        {
+            int code = CodeGenerator.Get();
+            Console.WriteLine($"Код подтверждения: {code} отправлен на почту: {e.Email}");
+        }
+
+        public void TurnOn()
+        {
+            userManager.UserRegistered += SendCode;
+        }
+        
+        public void TurnOff()
+        {
+            userManager.UserRegistered -= SendCode;
+        }
+    }
+
+    public class Advertisement
+    {
+        private readonly UserManager userManager;
+
+        public Advertisement(UserManager userManager)
+        {
+            this.userManager= userManager;
+        }
+
+        private void SendAds(object sender, UserRegisteredEventArgs e)
+        {
+            Console.WriteLine($"Последний день приобрести курс Технология Git по скидке. Отправлено на почту {e.Email}");
+        }
+
+        public void TurnOn()
+        {
+            userManager.UserRegistered += SendAds;
+        }
+
+        public void TurnOff()
+        {
+            userManager.UserRegistered -= SendAds;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            User user = new User();
-            user.MessageHandler += ShowOnConsole;
-            user.MessageHandler += SendEmail;
-            user.MessageHandler += User_MessageHandler;
-            user.SetAge(Console.ReadLine());
-        }
+            User user = new User("Ivan Ivanov", "ivanov@yandex.ru", "123321");
 
-        private static void User_MessageHandler(object sender, SetAgeEventArgs e)
-        {
-        }
+            UserManager userManager = new UserManager();
+            var advertisement = new Advertisement(userManager);
+            advertisement.TurnOn();
 
-        private static void ShowOnConsole(object sender, SetAgeEventArgs e)
-        {
-            Console.WriteLine($"Консоль: {e.Message}");
-        }
-
-        private static void SendEmail(object sender, SetAgeEventArgs e)
-        {
-            Console.WriteLine($"Почта: {e.Message}");
-        }
-
-        private static void SendSms(object sender, SetAgeEventArgs e)
-        {
-            Console.WriteLine($"Смс: {e.Message}");
+            userManager.Register(user);
         }
     }
 }

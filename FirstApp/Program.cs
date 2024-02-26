9e06e8abbeb8c44fecbbd2dfb3b6793c505fc442
Michael M. <trdMichaelM@yandex.ru>
2024-02-26 15:17:31 +0300

    Одинаковые книги

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,20 +4,15 @@ using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class Book : IComparable<Book>
+    public class Book : IEquatable<Book>
     {
         public string Name { get; set; }
         public string Author { get; set; }
         public int ReleaseYear { get; set; }
 
-        public int CompareTo(Book other)
+        public bool Equals(Book other)
         {
-            int compare = Author.CompareTo(other.Author);
-            if (compare == 0)
-            {
-                return ReleaseYear.CompareTo(other.ReleaseYear);
-            }
-            return compare;
+            return Name.Equals(other.Name) && Author.Equals(other.Author) && ReleaseYear.Equals(other.ReleaseYear);
         }
 
         public override string ToString()
@@ -26,48 +21,11 @@ namespace CSharpEssentials
         }
     }
 
-
     class Program
     {
         static void Main(string[] args)
         {
-            Book[] books = new Book[]
-            {
-                new Book
-                {
-                    Name = "Предел",
-                    Author = "Сергей Лукьяненко",
-                    ReleaseYear = 2021
-                },
 
-                new Book
-                {
-                    Name = "Порог",
-                    Author = "Сергей Лукьяненко",
-                    ReleaseYear = 2020
-                },
-
-                new Book
-                {
-                    Name = "Чистая архитектура",
-                    Author = "Мартин Роберт",
-                    ReleaseYear = 2018
-                },
-
-                new Book
-                {
-                    Name = "C# для профессионалов",
-                    Author = "Скит Джон",
-                    ReleaseYear = 2019
-                }
-            };
-
-            Array.Sort(books);
-
-            foreach (var book in books)
-            {
-                Console.WriteLine(book);
-            }
         }
     }
 }

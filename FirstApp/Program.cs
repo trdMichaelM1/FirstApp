55302c6ff5cafa8434f69c33c8a032b6a2dafc33
Michael M. <trdMichaelM@yandex.ru>
2024-02-26 14:43:47 +0300

    Сортировка по возрастанию

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,72 +4,64 @@ using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class Product
+    public class Book : IComparable<Book>
     {
-        public string Name { get; set; } // Имя 
-        public double Price { get; set; } // Цена
-        public int Quantity { get; set; } // Количество
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            return ReleaseYear.CompareTo(other.ReleaseYear);
+        }
 
         public override string ToString()
         {
-            return $"Название: {Name}, Цена: {Price}, Количество: {Quantity}";
+            return $"{Name}, {Author}, {ReleaseYear}";
         }
     }
 
-    public class Store : IEnumerable<Product>, IEnumerator<Product>
-    {
-        private readonly List<Product> products;
-        private int idx = -1;
 
-        public Store()
-        {
-            products = new List<Product>();
-        }
-
-        public void AddProduct(Product product)
-        {
-            products.Add(product);
-        }
-
-        public Product Current => products[idx];
-
-        object IEnumerator.Current => products[idx];
-
-        public IEnumerator<Product> GetEnumerator()
-        {
-            return this;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this;
-        }
-
-        public bool MoveNext()
-        {
-            idx++;
-            return idx < products.Count;
-        }
-
-        public void Reset()
-        {
-            idx = -1;
-        }
-
-        public void Dispose() { }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Store store = new Store();
-            store.AddProduct(new Product { Name = "Чайник", Price = 1000, Quantity = 3 });
-            store.AddProduct(new Product { Name = "Мультиварка", Price = 3000, Quantity = 7 });
-            store.AddProduct(new Product { Name = "Пылесос", Price = 7500, Quantity = 15 });
-
-            foreach (var product in store)
+            Book[] books = new Book[]
             {
-                Console.WriteLine(product);
+                new Book
+                {
+                    Name = "Предел",
+                    Author = "Сергей Лукьяненко",
+                    ReleaseYear = 2021
+                },
+
+                new Book
+                {
+                    Name = "Порог",
+                    Author = "Сергей Лукьяненко",
+                    ReleaseYear = 2020
+                },
+
+                new Book
+                {
+                    Name = "Чистая архитектура",
+                    Author = "Мартин Роберт",
+                    ReleaseYear = 2018
+                },
+
+                new Book
+                {
+                    Name = "C# для профессионалов",
+                    Author = "Скит Джон",
+                    ReleaseYear = 2019
+                }
+            };
+
+            Array.Sort(books);
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
             }
         }
     }

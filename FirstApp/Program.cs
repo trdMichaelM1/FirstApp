773505914613adfd11854f245d8f2064c43af34f
Michael M. <trdMichaelM@yandex.ru>
2024-02-28 13:22:59 +0300

    Перечисления

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,41 +1,27 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace CSharpEssentials
 {
-    public class Point : IEquatable<Point>
+    public enum Side
     {
-        public double Latitude { get; set; } // Широта
-        public double Longitude { get; set; } // Долгота
-
-        public override string ToString()
-        {
-            return $" Координаты точки: {Latitude} широты, {Longitude} долготы";
-        }
-
-        public Point(double Latitude, double Longitude)
-        {
-            this.Latitude = Latitude;
-            this.Longitude = Longitude;
-        }
-
-        public bool Equals(Point other)
-        {
-            int difference = 5;
-            return Math.Abs(other.Latitude - Latitude) <= difference && Math.Abs(other.Longitude - Longitude) <= difference;
-        }
+        North, // север
+        East,  // восток
+        South, // юг
+        West   // запад
     }
 
     class Program
     {
+        public static void GoTo(Side side, int meters)
+        {
+            Console.WriteLine($"Go {side} for {meters} meters");
+        }
         static void Main(string[] args)
         {
-            Point point1 = new Point(1, 2);
-            Point point2 = new Point(4, 2);
-            Point point3 = new Point(15, 5);
-            Console.WriteLine(point1.Equals(point2)); // True
-            Console.WriteLine(point1.Equals(point3)); // False
+            GoTo(Side.East, 50);
+            GoTo(Side.South, 20);
+            GoTo(Side.West, 30);
+            Console.WriteLine("You found it!");
         }
     }
 }

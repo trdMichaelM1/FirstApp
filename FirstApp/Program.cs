ff19f5064fc68cf07d0cb99af2c39fa49689cd7d
Michael M. <trdMichaelM@yandex.ru>
2024-03-01 11:35:35 +0300

    Инициирование исключений

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,39 +2,32 @@
 
 namespace CSharpEssentials
 {
+    public class Phone
+    {
+        public string Name { get; }
+        public decimal Cost { get; }
+        public Phone(string name, decimal cost)
+        {
+            // здесь поддержать ограничения параметров
+            if (name == null)
+                throw new ArgumentNullException("Имя не может быть равно null!");
+
+            if (name.Length < 2 || name.Length > 50)
+                throw new ArgumentException("Имя должно содержать от 2 до 50 символов!");
+
+            if (cost < 1)
+                throw new ArgumentException("Значение цены должно быть положительным!");
+            Name = name;
+            Cost = cost;
+        }
+    }
+
     class Program
     {
-        static void Do()
-        {
-            string[] data = Read();
-            // Do
-            Print(data);
-        }
-        static string[] Read()
-        {
-            string line = Console.ReadLine();
-            return line.Split();
-        }
-
-        static void Print(string[] array)
-        {
-            for (int i = 0; i <= array.Length; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
-        }
-
         static void Main(string[] args)
         {
-            try
-            {
-                int x = 10;
-                Do();
-            }
-            catch
-            {
-                Console.WriteLine("Упс! Произошло исключение!");
-            }
+
         }
+
     }
 }

763b05f169ebafff65f0ba703f0f2e4db3535e57
Michael M. <trdMichaelM@yandex.ru>
2024-03-01 13:06:39 +0300

    Восстановление после исключений

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,56 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace CSharpEssentials
 {
-    public class InvalidPasswordException : Exception
-    {
-        public InvalidPasswordException() { }
-        public InvalidPasswordException(string message) : base(message) { }
-    }
-
-    public class User
-    {
-        public string Name;
-        private string password;
-
-        public User(string name, string password) 
-        {
-            Name = name;
-            Password = password;
-        }
-        public string Password
-        {
-            get => password;
-            set
-            {
-                if (value.Length < 5)
-                    throw new InvalidPasswordException("Слишком короткий пароль");
-
-                password = value;
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
+            int a = 0;
             try
             {
-                User user = new User("Nik", "1234");
+                Console.WriteLine(1 / a);
             }
-            catch(InvalidPasswordException e)
+            catch (Exception)
             {
-                Console.WriteLine(e.Message); // Слишком короткий пароль
+                Console.WriteLine(2);
             }
-
-            try
+            finally
             {
-                User user = new User("May", "9736295637");
-                Console.WriteLine(user.Password); //9736295637
-            }
-            catch(InvalidPasswordException e)
-            {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(3);
             }
         }
 

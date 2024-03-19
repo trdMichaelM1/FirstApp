bd275fd8fae00a0580d0e538c1c9034818ffa29d
Michael M. <trdMichaelM@yandex.ru>
2024-03-19 16:58:23 +0300

    bool

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -3,48 +3,36 @@ using System.IO;
 using System.Text;
 
 namespace CSharpEssentials
-{
-    public class Team
-    {
-        public string Name { get; set; } = "Gryffindor";
-        public int Points { get; set; } = 0;
-
-        public void PrintInfo()
-        {
-            Console.WriteLine($"{Name}:{Points}");
-        }
-    }
-
-    public class Quidditch
-    {
-        public Team RedTeam = new Team();
-        public Team BlueTeam = new Team();
-
-        public void ShowScore()
-        {
-            Console.WriteLine($"{RedTeam.Name}:{RedTeam.Points}");
-            Console.WriteLine($"{BlueTeam.Name}:{BlueTeam.Points}");
-        }
-
-        public void RedSnitch()
-        {
-            RedTeam.Points += 150;
-            Console.WriteLine("Red Team Won!");
-        }
-
-        public void BlueSnitch()
-        {
-            BlueTeam.Points += 150;
-            Console.WriteLine("Blue Team Won!");
-        }
-    }
-
+{    
     class Program
     {
         static void Main(string[] args)
         {
             int size = sizeof(decimal);
             Console.WriteLine(size);
+
+            decimal x = 2M, y = 3M;
+
+            bool isEqual = x == y;
+            Console.WriteLine($"{x} == {y} = {isEqual}");
+
+            bool isNotEqual = x != y;
+            Console.WriteLine($"{x} != {y} = {isNotEqual}");
+
+            bool isLess = x < y;
+            Console.WriteLine($"{x} <  {y} = {isLess}");
+
+            bool isGreater = x > y;
+            Console.WriteLine($"{x} >  {y} = {isGreater}");
+
+            bool isLessOrEqual = x <= y;
+            Console.WriteLine($"{x} <= {y} = {isLessOrEqual}");
+
+            bool isGreaterOrEqual = x >= y;
+            Console.WriteLine($"{x} >= {y} = {isGreaterOrEqual}");
+
+            // Delay
+            Console.ReadKey();
         }
 
     }

30d832ff628349045e1131bee4ffb77695f261c3
Michael M. <trdMichaelM@yandex.ru>
2024-03-19 18:12:23 +0300

    ))

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -11,25 +11,14 @@ namespace CSharpEssentials
             int size = sizeof(decimal);
             Console.WriteLine(size);
 
-            decimal x = 2M, y = 3M;
+            int x1 = 10;
+            int x2 = 20;
+            (x1, x2) = (x2, x1);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
 
-            bool isEqual = x == y;
-            Console.WriteLine($"{x} == {y} = {isEqual}");
-
-            bool isNotEqual = x != y;
-            Console.WriteLine($"{x} != {y} = {isNotEqual}");
-
-            bool isLess = x < y;
-            Console.WriteLine($"{x} <  {y} = {isLess}");
-
-            bool isGreater = x > y;
-            Console.WriteLine($"{x} >  {y} = {isGreater}");
-
-            bool isLessOrEqual = x <= y;
-            Console.WriteLine($"{x} <= {y} = {isLessOrEqual}");
-
-            bool isGreaterOrEqual = x >= y;
-            Console.WriteLine($"{x} >= {y} = {isGreaterOrEqual}");
+            bool isTrue = true & false;
+            Console.WriteLine(isTrue);
 
             // Delay
             Console.ReadKey();
